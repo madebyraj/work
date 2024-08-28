479cb19ad39fbf7006a0a033e7d5930aaf8a2714
Raj <142151506+rajkumxr@users.noreply.github.com>
2024-08-28 21:55:40 +0530

    Nav enhancement

--- app/dynamic-nav.js
+++ app/dynamic-nav.js
@@ -16,7 +16,9 @@ const navLinkEls = document.querySelectorAll('.nav-link');
 const windowPathname = window.location.pathname;
 
 navLinkEls.forEach(navLinkEl => {
-  if (navLinkEl.href.includes (windowPathname)) {
+  const navLinkPathname = new URL(navLinkEl.href).pathname;
+
+  if ((windowPathname === navLinkPathname) || (windowPathname === '/index.html' && navLinkPathname === '/')) {
     navLinkEl.classList.add('current');
   }
 });
