792bf764a84d75c42e9a3efb6b87de89afbd6a7f
Raj <142151506+rajkumxr@users.noreply.github.com>
2024-08-28 13:45:02 +0530

    Links active state + bug fixes

--- app/dynamic-nav.js
+++ app/dynamic-nav.js
@@ -6,3 +6,17 @@ const navContainer = document.querySelector(".nav-container");
 menuIconContainer.addEventListener("click", () => {
   navContainer.classList.toggle("active");
 });
+
+
+
+
+// Link Active State
+
+const navLinkEls = document.querySelectorAll('.nav-link');
+const windowPathname = window.location.pathname;
+
+navLinkEls.forEach(navLinkEl => {
+  if (navLinkEl.href.includes (windowPathname)) {
+    navLinkEl.classList.add('current');
+  }
+});
