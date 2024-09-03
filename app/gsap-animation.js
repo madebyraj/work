eef0704f636b807ea38ea5d777fae3fff1a153ca
Raj <142151506+rajkumxr@users.noreply.github.com>
2024-09-03 15:26:07 +0530

    Animations updated

--- app/gsap-animation.js
+++ app/gsap-animation.js
@@ -19,8 +19,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
         delay: index * 0.2, // Delay each element based on its index
         scrollTrigger: {
           trigger: element,
-          start: "top 80%", // Start animation when the top of element is 85% from the top of the viewport
-          end: "top 20%", // End animation when the top of element is 20% from the top of the viewport
+          start: "top 85%", // Start animation when the top of element is 85% from the top of the viewport
+          end: "top 70%", // End animation when the top of element is 20% from the top of the viewport
           scrub: 1, // Smoothly scrub the animation as you scroll
         },
       }
@@ -38,14 +38,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
         {
           y: 0, // End with translation 0px
           opacity: 1, // End with opacity 1
-          duration: 1.5, // Duration of the animation
+          duration: 1, // Duration of the animation
           ease: "power2.out", // Ease out animation
           delay: index * 0.2, // Delay each element based on its index
           scrollTrigger: {
             trigger: element,
             start: "top 80%", // Start animation when the top of element is 85% from the top of the viewport
-            end: "top 20%", // End animation when the top of element is 20% from the top of the viewport
-            scrub: 1, // Smoothly scrub the animation as you scroll
+            end: "top 55%", // End animation when the top of element is 20% from the top of the viewport
+            scrub: 0.5, // Smoothly scrub the animation as you scroll
           },
         }
       );
