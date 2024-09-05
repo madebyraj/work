7bf50ac55774561497a31e62a88b8072d69abe26
Raj <142151506+rajkumxr@users.noreply.github.com>
2024-09-05 16:23:51 +0530

    Major bug fixed chat

--- app/gsap-animation.js
+++ app/gsap-animation.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         scrollTrigger: {
           trigger: element,
           start: "top 85%", // Start animation when the top of element is 85% from the top of the viewport
-          end: "top 70%", // End animation when the top of element is 20% from the top of the viewport
+          end: "top 75%", // End animation when the top of element is 20% from the top of the viewport
           scrub: 1, // Smoothly scrub the animation as you scroll
         },
       }
